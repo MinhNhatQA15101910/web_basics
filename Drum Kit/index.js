57a113cbccd48a9f9c1aaf5e1c9df8594b3a2a04
Do Nhat <dmnhat.pt@gmail.com>
2024-02-10 17:18:01 +0700

    Using keyboard event listener to check for key pressed.

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,32 +1,44 @@
 document.querySelectorAll(".drum").forEach((value) => {
   value.addEventListener("click", () => {
-    var playedAudio = "./sounds/";
-
-    switch (value.innerHTML) {
-      case "w":
-        playedAudio += "tom-1.mp3";
-        break;
-      case "a":
-        playedAudio += "tom-2.mp3";
-        break;
-      case "s":
-        playedAudio += "tom-3.mp3";
-        break;
-      case "d":
-        playedAudio += "tom-4.mp3";
-        break;
-      case "j":
-        playedAudio += "snare.mp3";
-        break;
-      case "k":
-        playedAudio += "crash.mp3";
-        break;
-      case "l":
-        playedAudio += "kick-bass.mp3";
-        break;
-    }
-
-    var audio = new Audio(playedAudio);
-    audio.play();
+    playSound(value.innerHTML);
   });
 });
+
+document.addEventListener("keydown", (event) => {
+  playSound(event.key);
+});
+
+function playSound(key) {
+  var src = "./sounds/";
+
+  switch (key) {
+    case "w":
+      src += "tom-1.mp3";
+      break;
+    case "a":
+      src += "tom-2.mp3";
+      break;
+    case "s":
+      src += "tom-3.mp3";
+      break;
+    case "d":
+      src += "tom-4.mp3";
+      break;
+    case "j":
+      src += "snare.mp3";
+      break;
+    case "k":
+      src += "crash.mp3";
+      break;
+    case "l":
+      src += "kick-bass.mp3";
+      break;
+    default:
+      src = "";
+  }
+
+  if (src.length !== 0) {
+    var audio = new Audio(src);
+    audio.play();
+  }
+}
