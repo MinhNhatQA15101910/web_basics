f39b62fbc13624569477feef76f77e70d27725ba
Do Nhat <dmnhat.pt@gmail.com>
2024-02-10 16:31:18 +0700

    Use switch case structure to control sounds.

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,5 +1,32 @@
 document.querySelectorAll(".drum").forEach((value) => {
   value.addEventListener("click", () => {
-    alert("I got clicked!");
+    var playedAudio = "./sounds/";
+
+    switch (value.innerHTML) {
+      case "w":
+        playedAudio += "tom-1.mp3";
+        break;
+      case "a":
+        playedAudio += "tom-2.mp3";
+        break;
+      case "s":
+        playedAudio += "tom-3.mp3";
+        break;
+      case "d":
+        playedAudio += "tom-4.mp3";
+        break;
+      case "j":
+        playedAudio += "snare.mp3";
+        break;
+      case "k":
+        playedAudio += "crash.mp3";
+        break;
+      case "l":
+        playedAudio += "kick-bass.mp3";
+        break;
+    }
+
+    var audio = new Audio(playedAudio);
+    audio.play();
   });
 });
