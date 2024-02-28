8564494b6d4d415441cd91acf06fbbbfe609324a
Do Nhat <dmnhat.pt@gmail.com>
2024-02-28 10:58:51 +0700

    Next part of Travel Tracker app.

--- PostgreSQL/Travel Tracker/index.js
+++ PostgreSQL/Travel Tracker/index.js
@@ -18,15 +18,62 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let error = "";
+
+async function isVisited(countryCode) {
+  const result = await db.query(
+    "SELECT * FROM visited_countries WHERE country_code = $1",
+    [countryCode]
+  );
+  return result.rowCount > 0;
+}
+
 app.get("/", async (req, res) => {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((row) => countries.push(row.country_code));
 
-  res.render("index.ejs", {
-    countries: countries,
-    total: result.rowCount,
-  });
+  if (error !== "") {
+    res.render("index.ejs", {
+      countries: countries,
+      total: result.rowCount,
+      error: error,
+    });
+  } else {
+    res.render("index.ejs", {
+      countries: countries,
+      total: result.rowCount,
+    });
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const userCountryName = req.body.country;
+
+  const queryResult = await db.query(
+    "SELECT country_code FROM countries WHERE UPPER(country_name) = UPPER($1)",
+    [userCountryName]
+  );
+
+  if (queryResult.rowCount <= 0) {
+    error = "Country name does not exist, try again.";
+    res.redirect("/");
+    return;
+  }
+
+  const countryCode = queryResult.rows[0].country_code;
+  if (await isVisited(countryCode)) {
+    error = "Country has already been added, try again.";
+    res.redirect("/");
+    return;
+  }
+
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+    countryCode,
+  ]);
+
+  error = "";
+  res.redirect("/");
 });
 
 app.listen(port, () => {
